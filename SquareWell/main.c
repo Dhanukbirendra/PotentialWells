28058786c4dcebd93de251d23b476949000ac9cb
Rajendra Adhikari <rpadhikari@gmail.com>
2018-11-17 19:45:29 +0545

    Update main.c

--- SquareWell/main.c
+++ SquareWell/main.c
@@ -1,1 +1,16 @@
 #include<stdio.h>
+
+FILE *out;
+double step(double);
+int main() {
+  int i, n=200;
+  double x, D=1.0, w=1.0;
+  w=w*0.5;
+  out=fopen("out.dat","w");
+  for(i=-n; i<=n; i++) {
+    x=0.01*(double) i;
+    fprintf(out,"%lf %lf\n",x, -D*(step(x+w)-step(x-w)));
+  }
+  fclose(out);
+}
+
